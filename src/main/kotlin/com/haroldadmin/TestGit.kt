9855923686efd26a888a6810abcef82c4b9cdeff
Yadong Liu <yadongl@tyro.com>
2022-05-17 15:41:38 +1000

    BIANG-78: [refactor] test3

--- src/main/kotlin/com/haroldadmin/TestGit.kt
+++ src/main/kotlin/com/haroldadmin/TestGit.kt
@@ -3,5 +3,5 @@ package com.haroldadmin
 public class TestGit {
     public var a : String = "cc"
     public var b : String = "bb"
-    public var c : String = "cc"
+    public var c : String = "aa"
 }
